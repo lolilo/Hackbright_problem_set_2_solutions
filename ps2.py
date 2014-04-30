87566768b637456e2ab75fb5606c14aeaba093ab
Hackbright Student <info@hackbrightacademy.com>
2014-04-30 15:52:01 -0700

    binary depth first search

--- ps2.py
+++ ps2.py
@@ -11,7 +11,7 @@ def binary_search(l, target):
         i = len(l)//2
         current_element = l[i]
         if target < current_element:
-            l = l[:i]
+            l = l[:i] # slicing the list each time is expensive...how can we avoid this?
         elif target > current_element:
             l = l[i + 1:]
         else: 
@@ -47,9 +47,25 @@ def fibonacci(n):
 
 """2.1 Implement a function to print the fibonacci numbers up to n, inclusive."""
 
+def print_fib(n):
+    a, b = 1, 1
+    print a
+    print b
+    for i in xrange(3, n + 1):
+        print a + b
+        temp = a
+        a = b
+        b += temp
+# print_fib(10)
+
 """3. Implement a function that recursively computes the length of a list
 
     [1, 2, 3] -> 3"""
+
+def recursive_length_of_list(l):
+    if not l:
+        return 0
+    return 1 + recursive_length_of_list(l[1:])
 
 """4. Binary search tree questions
     
@@ -63,6 +79,68 @@ def fibonacci(n):
 
     4.5 (GL4.1) Write a method, .balanced() that determines whether or not the tree is balanced and returns a boolean indicating the fact.
 """
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    def insert(self, node):
+        if node.val < self.val:
+            if not self.left:
+                self.left = node
+            else: # a node already exists
+                # check if existing node < new node; if so, insert here
+                if self.left.val < node.val:
+                    node.left = self.left
+                    self.left = node
+                else: # see where to insert new node past the existing node, recursively
+                    self.left.insert(node)
+        else: # node.val >= self.val
+            if not self.right:
+                self.right = node
+            else: 
+                if self.right.val > node.val:
+                    node.right = self.right
+                    self.right = node
+                else:
+                    self.right.insert(node)
+    def dft_preorder(self):
+        print self.val
+        if self.left:
+            self.left.dft_preorder()
+        if self.right:
+            self.right.dft_preorder()
+    def dft_postorder(self):
+        if self.left:
+            self.left.dft_preorder()
+        if self.right:
+            self.right.dft_preorder()
+        print self.val
+
+
+    
+# construct tree; see if you get desired result
+root = Node(8)
+five = Node(5)
+seven = Node(7)
+six = Node(6)
+eight = Node(8)
+nine = Node(9)
+ten = Node(10)
+
+# print root.val
+root.insert(five)
+# print root.left.val
+root.insert(six)
+# print root.left.val
+root.insert(eight)
+# print root.left.val, root.right.right.val
+root.insert(seven)
+# print root.left.val, root.right.right.right.val
+root.insert(nine)
+root.insert(ten)
+root.dft_postorder()
 
 """
 5. Write a function that, given two binary trees, returns a boolean indicating whether or not the trees have identical structure and value (ie: are they the same tree).
--- test_ps2.py
+++ test_ps2.py
@@ -5,10 +5,12 @@ class TestListOperations(unittest.TestCase):
 
     def setUp(self):
         self.l1 = [1, 2, 3, 4]
+        self.l_empty = []
 
     def test_binary_search(self):
         self.assertEqual(binary_search(self.l1, 5), False)
         self.assertEqual(binary_search(self.l1, 4), True)
+        self.assertEqual(binary_search(self.l_empty, 9), False)
 
     def test_recursive_binary_search(self):
         self.assertEqual(recursive_binary_search(self.l1, 5), False)
@@ -21,6 +23,11 @@ class TestListOperations(unittest.TestCase):
         self.assertEqual(fibonacci(3), 2)
         self.assertEqual(fibonacci(4), 3)
         self.assertEqual(fibonacci(10), 55)
+
+    def test_recursive_length_of_list(self):
+        self.assertEqual(recursive_length_of_list(self.l1), 4)
+        self.assertEqual(recursive_length_of_list(self.l_empty), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
